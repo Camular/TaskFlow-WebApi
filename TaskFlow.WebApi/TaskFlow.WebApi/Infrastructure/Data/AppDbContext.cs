6d2c2329a5cf5e1a333468cd9f3c97e0f880dd7c
musa fatih <musafatih69@gmail.com>
2026-07-16 15:00:01 +0300

    feat(data): wire ApplyConfigurationsFromAssembly in AppDbContext

--- TaskFlow.WebApi/TaskFlow.WebApi/Infrastructure/Data/AppDbContext.cs
+++ TaskFlow.WebApi/TaskFlow.WebApi/Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.X509Certificates;
 using TaskFlow.WebApi.Core.Entities;
 
 namespace TaskFlow.WebApi.Infrastructure.Data
@@ -20,23 +19,9 @@ namespace TaskFlow.WebApi.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<UserWorkspaceRole>()
-                .HasIndex(uwr => new { uwr.UserId, uwr.RoleId, uwr.WorkspaceId })
-                .IsUnique();
 
-            modelBuilder.Entity<WorkspaceRole>()
-                .HasIndex(wr => new { wr.Name })
-                .IsUnique()
-                .HasFilter("\"WorkspaceId\" IS NULL");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 
-            modelBuilder.Entity<WorkspaceRole>()
-                .HasIndex(wr => new { wr.Name, wr.WorkspaceId })
-                .IsUnique()
-                .HasFilter("\"WorkspaceId\" IS NOT NULL");
-
-            modelBuilder.Entity<RolePermission>()
-                .HasIndex(rp => new { rp.RoleId, rp.PermissionId })
-                .IsUnique();
         }
 
     }
