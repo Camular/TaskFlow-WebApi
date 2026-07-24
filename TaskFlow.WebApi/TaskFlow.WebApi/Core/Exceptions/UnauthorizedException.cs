a3d7d46350d565d7bea20ad202f3af68da1a3633
musa fatih <musafatih69@gmail.com>
2026-07-24 23:20:47 +0300

    feat(core): add UnauthorizedException and ForbiddenException for typed API errors

---
+++ TaskFlow.WebApi/TaskFlow.WebApi/Core/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskFlow.WebApi.Core.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException(string message) : base(message)
+        {
+        }
+    }
+}
